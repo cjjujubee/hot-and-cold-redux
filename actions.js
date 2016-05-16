0941461f467e28e42f02be9d30cd2559bb6e8c6f
cjjujubee <connie.jew@gmail.com>
2016-05-16 08:48:03 -0700

    Adds actions for Hot and Cold game

---
+++ actions.js
@@ -0,0 +1,23 @@
+var NEW_GAME = 'NEW_GAME';
+var newGame = function() {
+  return {
+    type: NEW_GAME,
+  }
+};
+
+var SHOW_INSTRUCTIONS = 'SHOW_INSTRUCTIONS';
+var showInstructions = function() {
+  return {
+    type: SHOW_INSTRUCTIONS,
+    instructions: 'Make a guess.'
+  }
+}
+
+//makes guess and validates
+var MAKE_GUESS = 'MAKE_GUESS';
+var makeGuess = function(guess) {
+  return {
+    type: MAKE_GUESS,
+    guess: guess
+  }
+};
